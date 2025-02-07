d42373daf9bf350475b9109fcd200dc51b7f4d84
JuliaBughi <ju.bughi@gmail.com>
2025-02-07 21:47:51 +0000

    alterações no código

--- TPC1/tpc1.py
+++ TPC1/tpc1.py
@@ -3,36 +3,36 @@ import sys
 def somador_onoff(texto):
     isOn = True
     acc = 0
-    i=0
+    i = 0
 
-    while i<len(texto):
+    while i < len(texto):
         valor = 0
 
-        if texto[i] in "0123456789":
+        if texto[i] in "0123456789" and isOn:
             while texto[i] in "0123456789":
                 valor = valor * 10 + int(texto[i])
-                i = i+1
-            if isOn:
-                acc += valor
+                i += 1
 
+            acc += valor
+        
         elif texto[i] in "Oo":
-            if texto[i+1] in "Nn":
+            if i + 1 < len(texto) and texto[i+1] in "Nn":
                 isOn = True
-
-            elif texto[i+1] in "Ff":
-                if texto[i+2] in "Ff":
-                    isOn = False
-
-            i = i+1
-
-        elif texto[i] in "=":
+                i += 2
+            
+            elif i + 2 < len(texto) and texto[i+1] in "Ff" and texto[i+2] in "Ff":
+                isOn = False
+                i += 3
+            
+            else:
+                i += 1
+        
+        elif texto[i] == "=":
             print(acc)
-            i = i+1
-
+            i += 1
+        
         else:
-            i = i+1
+            i += 1
 
 for linha in sys.stdin:
     somador_onoff(linha)
-
-
